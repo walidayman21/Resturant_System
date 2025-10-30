8f7047164ce0c2ff3d0487dec6d8e355421357bc
Walid Ayman Othman <walidayman21@gmail.com>
2025-10-30 23:18:09 +0200

    2

--- Resturant_System/Controllers/HomeController.cs
+++ Resturant_System/Controllers/HomeController.cs
@@ -9,15 +9,21 @@ namespace Resturant_System.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             _logger = logger;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public IActionResult Index()
         {
-            //_signInManager.SignOutAsync();
+            _signInManager.SignOutAsync();
+
             return View();
         }
 
